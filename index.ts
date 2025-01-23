0c328722f235333ac97e03d310d1484a7f16ca73
Selasie5 <selasisepenu5@gmail.com>
2025-01-23 16:48:07 +0000

    feat:implement schema, query for getting all user data and mutation for adding a user to the database

--- index.ts
+++ index.ts
@@ -1,9 +1,76 @@
 import express from "express";
+import * as UserData from "./MOCK_DATA.json"
 import { Request, Response } from "express";
+import graphql, { GraphQLSchema,GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList } from "graphql"
+import { graphqlHTTP } from "express-graphql";
 
 const app = express();
 
 app.use(express.json())
+
+//Create type defintition for user
+const UserType=  new GraphQLObjectType(
+    {
+        name: "user",
+        fields: ()=>({
+            id:{type:GraphQLInt},
+            firstName:{type:GraphQLString},
+            lastName:{type:GraphQLString},
+            email:{type:GraphQLString},
+            password:{type:GraphQLString},
+        })
+    }
+)
+
+//Implement the root query for getting all users
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields:{
+       getAllUsers:{
+        type: new GraphQLList(UserType),
+        args:{
+
+        },
+        resolve:(parent,args)=> {
+            return UserData
+        }
+       }
+    }
+})
+
+//Implement a mutation for the 
+const Mutation = new GraphQLObjectType({
+    name:"Mutation",
+    fields:{
+        createUser: {
+            type: UserType,
+            args: {
+                firstName: {type: GraphQLString},
+                lastName: {type: GraphQLString},
+                email: {type: GraphQLString},
+                password: {type: GraphQLString},
+                
+            },
+            resolve:(parent, args)=>
+            {
+                UserData.push({
+                    id:UserData.length +1,
+                    firstName: args.firstName,
+                    lastName: args.lastName,
+                    email:args.email,
+                    password:args.password
+                })
+                return args
+            }
+        }
+    }
+})
+const schema =  new GraphQLSchema({query:RootQuery ,mutation:Mutation})
+
+app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: true
+}))
 
 
 app.get("/", (req:Request, res:Response)=>
@@ -13,5 +80,5 @@ app.get("/", (req:Request, res:Response)=>
 
 app.listen(3000,()=>
 {
-    console.log("🎉✅ Server is runnong on PORT 3000")
+    console.log("🎉✅ Server is running on PORT 3000")
 })
